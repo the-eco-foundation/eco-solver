c4ff7c1ee7226ea39ab7649b3987872e6bb7306c
Stoyan Dimitrov <s.dimitrov19@gmail.com>
2024-08-27 15:57:15 -0400

    update preprod vals for stage 1 tests

--- config/preproduction.ts
+++ config/preproduction.ts
@@ -19,6 +19,15 @@ export default {
       sourceAddress: '0x532BA2D408e77B773b1d05Dafa5E4A2392e5ED11',
       tokens: [
         '0x0b2c639c533813f4aa9d7837caf62653d097ff85', //usdc
+        '0x94b008aA00579c1307B0EF2c499aD98a8ce58e58', //usdt
+      ],
+    },
+    {
+      network: 'base-mainnet',
+      chainID: 8453,
+      sourceAddress: '0x5e46855a436FDc16342EB0689f6555Db59b0245B',
+      tokens: [
+        '0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913', //usdc
       ],
     },
   ],
