a3722f46adc0f9191bccccdeb663ad286630b388
Stoyan Dimitrov <s.dimitrov19@gmail.com>
2024-09-25 15:09:22 -0400

    clean up

--- config/production.ts
+++ config/production.ts
@@ -4,33 +4,4 @@ export default {
     secretID: 'eco-solver-secrets-prod',
   },
   //don't add anything else here
-  logger: {
-    usePino: false,
-  },
-  database: {
-    auth: {
-      enabled: false,
-      username: '',
-      password: '',
-      type: '',
-    },
-
-    uriPrefix: 'mongodb://',
-    uri: 'localhost:27017',
-    dbName: 'eco-solver-local',
-    enableJournaling: true,
-  },
-  redis: {
-    connection: {
-      host: 'localhost',
-      port: 6379,
-    },
-    jobs: {
-      //remove on complete/fail for dev so we can submit the same tx multiple times
-      intentJobConfig: {
-        removeOnComplete: true,
-        removeOnFail: true,
-      },
-    },
-  },
 }
