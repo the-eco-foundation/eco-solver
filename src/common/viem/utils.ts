132b473d2b21038e4cac1723c2639de594e265c1
Stoyan Dimitrov <s.dimitrov19@gmail.com>
2024-10-17 12:16:06 -0400

    Ed 4172 non alchemy chains (#47)
    * moving utils, begining to refactor chain definitions
    
    * removing @alchemy/aa-core
    created a supported array of chains
    defined helix and eco test chains
    refactored everything to not use aa-core chains
    
    * adding unit tests for chain geturl
    prettify some types
    
    * fix name conventions for custom chains

--- src/common/viem/utils.ts
+++ src/common/viem/utils.ts
@@ -1,17 +1,46 @@
-import { getAddress, Hex } from 'viem'
+import { Chain, getAddress, Hex } from 'viem'
+
+/**
+ * Gets the url for a chain with the given api key, either websocket or http. It tries
+ * to find a non default rpc url if it exists and returns the first one it finds,
+ * otherwise it returns the default one.
+ *
+ * @param chain the chain to get the url for
+ * @param apiKey the api key if it is required
+ * @param websocketEnabled whether to try the websocket url if there is one
+ * @returns
+ */
+export function getRpcUrl(
+  chain: Chain,
+  apiKey?: string,
+  websocketEnabled: boolean = false,
+): { url: string; isWebsocket: boolean } {
+  let rpcUrl = chain.rpcUrls.default
+  for (const key in chain.rpcUrls) {
+    if (key === 'default') {
+      continue
+    }
+    rpcUrl = chain.rpcUrls[key]
+    break
+  }
+  const isWebsocket =
+    websocketEnabled && rpcUrl.webSocket != undefined && rpcUrl.webSocket.length > 0
+  const url = isWebsocket ? rpcUrl.webSocket![0] : rpcUrl.http[0]
+
+  return {
+    url: apiKey ? url + '/' + apiKey : url,
+    isWebsocket,
+  }
+}
+
 /**
  * Lowercase all top-level keys of the given `object` to lowercase.
  *
  * @returns {Object}
  */
-export function addressKeys(obj: Record<Hex, any>): Record<Hex, any> | undefined {
-  if (!obj) {
-    return undefined
-  }
-
+export function addressKeys(obj: Record<Hex, any>): Record<Hex, any> {
   return Object.entries(obj).reduce((carry, [key, value]) => {
     carry[getAddress(key)] = value
-
     return carry
   }, {})
 }
