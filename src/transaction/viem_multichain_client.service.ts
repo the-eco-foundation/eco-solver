2019b5a0199a987c103f435406c2e5fed6664674
Stoyan Dimitrov <s.dimitrov19@gmail.com>
2024-10-24 16:48:59 -0400

    comment

--- src/transaction/viem_multichain_client.service.ts
+++ src/transaction/viem_multichain_client.service.ts
@@ -67,6 +67,7 @@ export class ViemMultichainClientService<T extends Client, V extends ClientConfi
   }
 
   protected async buildChainConfig(chain: Chain): Promise<V> {
+    //only pass api key if chain is supported by alchemy, otherwise it'll be incorrectly added to other rpcs
     const apiKey = this.supportedAlchemyChainIds.includes(chain.id) ? this.apiKey : undefined
     const rpcTransport = getTransport(chain, apiKey)
     return {
