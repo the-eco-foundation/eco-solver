b3a08eedccc1d32f46de6183ff8de07529e4072a
Carlos Febres <cfebres@eco.com>
2024-10-31 00:34:56 -0400

    add Mantle and Arbitrum

--- src/common/chains/supported.ts
+++ src/common/chains/supported.ts
@@ -1,17 +1,21 @@
 import { Chain } from 'viem'
-// import { ecoSepolia } from './definitions/eco'
-// import { helix } from './definitions/helix'
+import { ecoSepolia } from './definitions/eco'
+import { helix } from './definitions/helix'
 import { optimism, optimismSepolia } from './definitions/optimism'
 import { base, baseSepolia } from './definitions/base'
+import { arbitrum } from './definitions/arbitrum'
+import { mantle } from './definitions/mantle'
 
 /**
  * List of supported chains for the solver that have modified RPC URLs or are defined in the project
  */
 export const ChainsSupported: Chain[] = [
-  // ecoSepolia,
-  // helix,
   optimism,
   optimismSepolia,
   base,
   baseSepolia,
+  ecoSepolia,
+  helix,
+  arbitrum,
+  mantle,
 ]
