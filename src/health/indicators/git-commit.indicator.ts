1575c692ed7bfd80bdcf8f13c4a6aa7393985a1e
Stoyan Dimitrov <s.dimitrov19@gmail.com>
2024-12-04 16:54:48 -0500

    lint

--- src/health/indicators/git-commit.indicator.ts
+++ src/health/indicators/git-commit.indicator.ts
@@ -3,6 +3,7 @@ import { HealthIndicator, HealthIndicatorResult } from '@nestjs/terminus'
 import { exec } from 'child_process'
 import { readFileSync } from 'fs'
 import { join } from 'path'
+import { EcoLogMessage } from '../../common/logging/eco-log-message'
 
 const ECO_ROUTES_PACKAGE_NAME = '@eco-foundation/routes'
 @Injectable()
@@ -14,7 +15,10 @@ export class GitCommitHealthIndicator extends HealthIndicator {
 
   async gitCommit(): Promise<HealthIndicatorResult> {
     const npmLib = this.getDependencyVersion(ECO_ROUTES_PACKAGE_NAME)
-    return this.getStatus('git-commit', !!npmLib, { commitHash: await this.getCommitHash(), ecoRoutesVersion: npmLib })
+    return this.getStatus('git-commit', !!npmLib, {
+      commitHash: await this.getCommitHash(),
+      ecoRoutesVersion: npmLib,
+    })
   }
 
   private async getCommitHash(): Promise<string> {
@@ -29,24 +33,34 @@ export class GitCommitHealthIndicator extends HealthIndicator {
     })
   }
 
-  private getDependencyVersion(dependencyName: string): { version: string, npm: string } | undefined {
+  private getDependencyVersion(
+    dependencyName: string,
+  ): { version: string; npm: string } | undefined {
     try {
       // Path to the project's package.json file
-      const packageJsonPath = join(process.cwd(), "package.json")
-  
+      const packageJsonPath = join(process.cwd(), 'package.json')
+
       // Read and parse the package.json file
-      const packageJson = JSON.parse(readFileSync(packageJsonPath, "utf-8"))
+      const packageJson = JSON.parse(readFileSync(packageJsonPath, 'utf-8'))
 
       // Check dependencies and devDependencies for the specified dependency
-      let version = packageJson.dependencies?.[dependencyName] ||
-      packageJson.devDependencies?.[dependencyName] ||
-      'undefined'
+      const version =
+        packageJson.dependencies?.[dependencyName] ||
+        packageJson.devDependencies?.[dependencyName] ||
+        'undefined'
       return {
         version,
-        npm: `https://www.npmjs.com/package/${dependencyName}/v/${version.replace('^', '')}?activeTab=code`
+        npm: `https://www.npmjs.com/package/${dependencyName}/v/${version.replace('^', '')}?activeTab=code`,
       }
     } catch (error) {
-      console.error("Error reading package.json:", error)
+      this.logger.error(
+        EcoLogMessage.fromDefault({
+          message: 'Error reading package.json:',
+          properties: {
+            error,
+          },
+        }),
+      )
       return undefined // Return undefined if there is an error
     }
   }
